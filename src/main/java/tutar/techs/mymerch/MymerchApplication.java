83fbac75666b32ebc502fae2016cdc2478ed758b
Emir <etutar42@gmail.com>
2026-09-14 17:14:25 +0200

    Add dotenv

--- src/main/java/tutar/techs/mymerch/MymerchApplication.java
+++ src/main/java/tutar/techs/mymerch/MymerchApplication.java
@@ -1,5 +1,6 @@
 package tutar.techs.mymerch;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MymerchApplication {
 
 	public static void main(String[] args) {
+		// Configure dotenv to load environment variables
+		Dotenv dotenv = Dotenv.configure()
+				.directory(".")
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		
+		
 		SpringApplication.run(MymerchApplication.class, args);
 	}
 
