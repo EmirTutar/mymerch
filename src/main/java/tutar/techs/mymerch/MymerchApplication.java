47f0238b5cdab5a358443cd93d205ec822a80ec0
Emir <etutar42@gmail.com>
2026-09-14 16:14:51 +0200

    Initial commit

---
+++ src/main/java/tutar/techs/mymerch/MymerchApplication.java
@@ -0,0 +1,13 @@
+package tutar.techs.mymerch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymerchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymerchApplication.class, args);
+	}
+
+}
